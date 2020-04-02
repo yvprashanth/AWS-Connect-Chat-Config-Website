b9fd190f036f7827c082b3c20a06483d41cee15b
Prashanth Yerramilli <yerramp@amazon.com>
2020-04-02 13:32:55 -0700

    Initial Version

---
+++ src/containers/FormContainer.jsx
@@ -0,0 +1,199 @@
+import React, { Component } from "react";
+
+/* Import Components */
+import CheckBox from "../components/CheckBox";
+import Input from "../components/Input";
+import TextArea from "../components/TextArea";
+import Select from "../components/Select";
+import Button from "../components/Button";
+
+class FormContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newUser: {
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: ""
+      },
+
+      genderOptions: ["Male", "Female", "Others"],
+      skillOptions: ["Programming", "Development", "Design", "Testing"]
+    };
+    this.handleTextArea = this.handleTextArea.bind(this);
+    this.handleAge = this.handleAge.bind(this);
+    this.handleFullName = this.handleFullName.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleClearForm = this.handleClearForm.bind(this);
+    this.handleCheckBox = this.handleCheckBox.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  /* This lifecycle hook gets executed when the component mounts */
+
+  handleFullName(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          name: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleAge(e) {
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          age: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleInput(e) {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          [name]: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleTextArea(e) {
+    console.log("Inside handleTextArea");
+    let value = e.target.value;
+    this.setState(
+      prevState => ({
+        newUser: {
+          ...prevState.newUser,
+          about: value
+        }
+      }),
+      () => console.log(this.state.newUser)
+    );
+  }
+
+  handleCheckBox(e) {
+    const newSelection = e.target.value;
+    let newSelectionArray;
+
+    if (this.state.newUser.skills.indexOf(newSelection) > -1) {
+      newSelectionArray = this.state.newUser.skills.filter(
+        s => s !== newSelection
+      );
+    } else {
+      newSelectionArray = [...this.state.newUser.skills, newSelection];
+    }
+
+    this.setState(prevState => ({
+      newUser: { ...prevState.newUser, skills: newSelectionArray }
+    }));
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    let userData = this.state.newUser;
+
+    fetch("http://example.com", {
+      method: "POST",
+      body: JSON.stringify(userData),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(response => {
+      response.json().then(data => {
+        console.log("Successful" + data);
+      });
+    });
+  }
+
+  handleClearForm(e) {
+    e.preventDefault();
+    this.setState({
+      newUser: {
+        name: "",
+        age: "",
+        gender: "",
+        skills: [],
+        about: ""
+      }
+    });
+  }
+
+  render() {
+    return (
+      <form className="container-fluid" onSubmit={this.handleFormSubmit}>
+        <Input
+          inputType={"text"}
+          title={"Connect Instance ARN"}
+          name={"name"}
+          value={this.state.newUser.name}
+          placeholder={"Enter your Instance ARN"}
+          handleChange={this.handleInput}
+        />{" "}
+        {/* Name of the user */}
+        <Input
+          inputType={"text"}
+          name={"age"}
+          title={"Contact Contact Flow ARN"}
+          value={this.state.newUser.age}
+          placeholder={"Enter your Contact Center Flow ID"}
+          handleChange={this.handleAge}
+        />{" "}
+        {/* About you */}
+        <Input
+          inputType={"text"}
+          name={"age"}
+          title={"IAM Role"}
+          value={this.state.newUser.age}
+          placeholder={"IAM User Role"}
+          handleChange={this.handleAge}
+        />
+        <Input
+          inputType={"text"}
+          name={"age"}
+          title={"Domain that needs to be whitelisted"}
+          value={this.state.newUser.age}
+          placeholder={"Please enter a domain that needs to be whitelisted"}
+          handleChange={this.handleAge}
+        />
+        <Button
+          action={this.handleFormSubmit}
+          type={"primary"}
+          title={"Submit"}
+          style={buttonStyle}
+        />{" "}
+        {/*Submit */}
+        <Button
+          action={this.handleClearForm}
+          type={"secondary"}
+          title={"Clear"}
+          style={buttonStyle}
+        />{" "}
+        {/* Clear the form */}
+      </form>
+    );
+  }
+}
+
+const buttonStyle = {
+  margin: "10px 10px 10px 10px"
+};
+
+export default FormContainer;
